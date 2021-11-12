37545c6513c851a5229dcea70aeeae3728a1078e
mharoonj <mharoon.j@hotmail.com>
2021-11-12 11:41:51 +0500

    strings

--- helloworld/app.py
+++ helloworld/app.py
@@ -1,16 +1,16 @@
-price = 10
-name = "Haroon"
-is_published = True
+birthday_year= "1998"
+# print(type(birthday_year))
+age = 2019 - int(birthday_year)
+# print (age)
 
 
+# strings
 
-#exercise 
-name = "John Smith"
-age  = 20
-is_new = True
+course = "python for beginners"
+print(course[:-10])
+print(course[:]) #cloning a string
 
-#recieving input
-name = input("What is your name ? ")
-fav_color = input("what is your favourite color? ")
-print(name+ " likes " +  fav_color)
 
+#string exercise
+name = "Jennifer"
+print(name[1:-1])
